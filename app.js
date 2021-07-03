4f227621274b233dc79a77c1d349afce773a60b7
zeqas <casperi52400@gmail.com>
2021-07-03 18:34:42 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose') // 載入 mongoose
 
 // 設定首頁路由
+
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
