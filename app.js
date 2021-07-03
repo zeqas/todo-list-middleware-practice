d42836757b11153cd92b1062fb0bc2f4dc3fe556
zeqas <casperi52400@gmail.com>
2021-07-03 21:53:03 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
 
-// 設定首頁路由
+const app = express()
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,8 +21,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 樣板引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 設定路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
