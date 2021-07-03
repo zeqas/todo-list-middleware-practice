4d4e3b6f0713ae12c89391b9a8dc422c500a995d
zeqas <casperi52400@gmail.com>
2021-07-03 21:34:55 +0800

    feat: add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo') // 載入 todo model
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+  console.log('done')
+})
