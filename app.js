3522241ed2c61e6dc70f3ad417fb203037bc1526
zeqas <casperi52400@gmail.com>
2021-07-05 12:01:14 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -77,6 +77,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
