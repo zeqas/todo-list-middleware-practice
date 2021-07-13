ec08ff93be7bfb5c565440647dc778a9afae35da
zeqas <casperi52400@gmail.com>
2021-07-13 13:43:38 +0800

    fix: route's bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const home = require('./modules/home')
 
-const todos = require('./modules/todos')
+const todos = require('./modules/todo')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
