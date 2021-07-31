4cb92d40f0d855a27b58c806095c6d8e668b7f7e
zeqas <casperi52400@gmail.com>
2021-07-31 14:57:30 +0800

    feat: middleware and delete the commits of todo.js

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -4,66 +4,27 @@ const router = express.Router()
 const Todo = require('../../models/todo')
 
 const dateToString = require('../../tools/dateToString')
-///// ----- Middleware-practice ----- /////
-
-// 每當應用程式收到要求時，就會執行此函數
-// getTime() : JS的時間戳  
-// router.use('/', function (req, res, next) {
-  
-  
-  // const date = new Date.now
-  // console.log(date)
-
-//   console.log( dateToString(date) + " | " + `${req.method} from ${req.originalUrl}`)
-//   next()
-// })
-
-// 對 /:id 路徑上任何類型的 HTTP 要求，執行此函數
-// Request URL: /todos/60e0662c9b02b91f0c0feb38
-// Request Type: GET
-
-// router.use('/:id', function (req, res, next) {
-//   console.log('Request URL:', req.originalUrl)
-//   next()
-// }, function (req, res, next) {
-//   next()
-// })
-
-
-// 路由處理程式可為 一個路徑 定義 多個路由
-// 下列範例為指向 /:id 路徑的 GET 要求，定義兩個路由
-// 第二個路由不會造成任何問題，卻絕不會呼叫，因為第一個路由會結束要求/回應循環
-
-// router.get('/:id', function (req, res, next) {
-//   // if the ID is 0, skip to the next router
-//   if (req.params.id === 0) next('route')
-//   else next() 
-// }, function (req, res, next) {
-//   console.log('ID:', req.params.id) 
-//   next()
-// }, function (req, res, next) {
-//   res.send('User Info') // 顯示字串
-//   next() // 無效
-// })
-
-// 結束並且印出req.params.id
-// router.get('/:id', function (req, res, next) {
-//   res.end(req.params.id)
-// })
-
-// ** 目標一：印出 時間戳記  req.method  URL
-
-// 時間戳記 time-stamp
 
 router.use('/', function (req, res, next) {
-  // 從 Date 類別建立一個新的物件，return建立的時間
-  let date = new Date 
+  const method = req.method
+  const timeStart = new Date()
 
-  console.log(dateToString(date) + " | " + `${req.method} from ${req.originalUrl}`)
+  const date = timeStart.toISOString().slice(0, 10)
+  const time = timeStart.toLocaleTimeString('en-US', { timeStyle: 'medium', hour12: false })
+
+  const url = req.url
+
+  res.locals.logs = { method: method, date: date, time: time, url: url }
+
+  res.on('finish', () => {
+    const timeEnd = new Date()
+    const ping = timeEnd - timeStart
+
+    const message = `${date} ${time} | ${method} from ${url} | total time: ${ping}ms`
+    console.log(message)
+  })
   next()
 })
-
-///// ----- Middleware-practice END----- /////
 
 // Create
 router.get('/new', (req, res) => {
