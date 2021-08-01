fcf3d5500ff3d897d7d4a5515fa793b892b70069
zeqas <casperi52400@gmail.com>
2021-08-01 15:16:51 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const session = require('express-session')
 // 引用路由器
 const routes = require('./routes')
 require('./config/mongoose')
@@ -15,6 +15,12 @@ const methodOverride = require('method-override')
 // 樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // setting body-parser
 app.use(express.urlencoded({ extended: true })) //改寫成 express
