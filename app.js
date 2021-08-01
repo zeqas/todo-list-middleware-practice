8d1f648628be4eb2709bcb30f0560563c988179e
zeqas <casperi52400@gmail.com>
2021-08-01 16:36:36 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 // 引用路由器
 const routes = require('./routes')
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
 require('./config/mongoose')
 
 const app = express()
@@ -27,6 +31,9 @@ app.use(express.urlencoded({ extended: true })) //改寫成 express
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 // 將 request 導入路由器
 app.use(routes)
