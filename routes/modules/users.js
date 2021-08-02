18ae3ab06c260a56988afa3f1cac5c0be5e32b95
zeqas <casperi52400@gmail.com>
2021-08-02 14:49:53 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const User = require('../../models/user')
 
+const passport = require('passport')
+const bcrypt = require('bcryptjs')  // 載入套件
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -48,13 +51,16 @@ router.post('/register', (req, res) => {
       })
     } 
     
-    return User.create({
-      name,
-      email,
-      password
-    })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash // 用雜湊值取代原本的使用者密碼
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
 })
 
